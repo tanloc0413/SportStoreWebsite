5edf514ceb2c29b8049a29abbfb14ce7eaffa4ed
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2026-02-10 18:00:50 +0700

    .

--- back-end/src/main/java/com/fit/backend/dto/OrderDetail.java
+++ back-end/src/main/java/com/fit/backend/dto/OrderDetail.java
@@ -1,13 +1,25 @@
 package com.fit.backend.dto;
 
+import com.fit.backend.entity.Address;
+import com.fit.backend.entity.OrderStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class OrderDetail {
+    private Integer id;
+    private Date orderDate;
+    private Address address;
+    private Double totalAmount;
+    private OrderStatus orderStatus;
+    private String shipmentNumber;
+    private Date expectedDeliveryDate;
+//    private List<OrderItemDetail> orderItemList;
 }
